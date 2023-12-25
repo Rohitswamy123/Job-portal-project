e36c24e1d0d3ca7311ba6e160f10bd5857342fa8
rohit <sbhartiya@me.iitr.ac.in>
2023-12-25 21:14:10 +0530

    CRUD funtionality of api with add get edit functionality

--- server.js
+++ server.js
@@ -49,6 +49,45 @@ app.post("/api/v1/jobs", (req, res) => {
   res.status(200).json({ job });
 });
 
+// get single job
+
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  console.log(`${job}`);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  res
+    .status(200)
+    .json({ job: `org is ${job.company}`, position: `${job.position}` });
+});
+
+// edit job
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    return res
+      .status(401)
+      .json({ msg: "please enter details of org and position" });
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  console.log(`${job}`);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  job.company = company;
+  job.position = position;
+
+  res
+    .status(200)
+    .json({ job: `org is ${job.company}`, position: `${job.position}` });
+});
+
 app.listen(5100, () => {
   console.log("app is listening on port 5100");
 });
